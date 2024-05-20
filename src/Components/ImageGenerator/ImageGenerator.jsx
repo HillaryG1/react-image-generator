4ca125f32ec0dc8b621b50ca2fbf8769dfd2038b
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-05-20 17:20:14 -0400

    Implement Styling for Image

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -6,7 +6,11 @@ export const ImageGenerator = () => {
   return (
     <div className='ai-image-generator'>
       <div className="header">Ai image <span>generator</span></div>
-      <div className='img-loading'></div>
+      <div className='img-loading'>
+        <div className="image"><img src={default_image} alt="" /></div>
+
+      </div>
+      <div className="search-box"></div>
 
     </div>
   )
