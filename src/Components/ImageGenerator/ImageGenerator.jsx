12be5062829a107a1907e5510e01e63b340b4116
Hillary X <hillaryx@hillarys-air.mynetworksettings.com>
2024-05-20 19:31:36 -0400

    Implement API

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './ImageGenerator.css'
 import default_image from '../Assets/default image.svg'
 
 export const ImageGenerator = () => {
+
+  const [image_url, setImage_url] = useState("/")
+  let inputRef = useRef(null);
+
+  const ImageGenerator = async () => {
+    if (inputRef.current.value==="") {
+      return 0;
+    }
+    const response = await fetch(
+      " https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+            Authorization:
+            `Bearer ${process.env.API_KEY}`,
+            "User-Agent":"Chrome",
+        },
+        body:JSON.stringify({
+          prompt: `${inputRef.current.value}`,
+          n:1,
+          size:"512x512",
+        }),
+      }
+    );
+    let data = await response.json();
+    let data_array = data.data;
+    setImage_url(data_array[0].url);
+    console.log(data);
+  }
   return (
     <div className='ai-image-generator'>
       <div className="header">Ai image <span>generator</span></div>
       <div className='img-loading'>
-        <div className="image"><img src={default_image} alt="" /></div>
+        <div className="image"><img src={image_url==="/"?default_image:image_url} alt="" /></div>
+        <div className="loading">
+          <div className="loading-bar"></div>
+        </div>
 
       </div>
       <div className="search-box">
-        <input type="text" className='search-input' placeholder='Describe the Image You Want' />
-        <div className="generate-btn">Generate</div>
+        <input type="text" ref={inputRef} className='search-input' placeholder='Describe the Image You Want' />
+        <div className="generate-btn"  onClick={ImageGenerator}>Generate</div>
       </div>
     </div>
   )
